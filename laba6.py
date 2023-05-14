06e9892ae1da40542e1aa6a85fcd642f6b7b4d3c
AnnaKashitsyna29 <125042682+AnnaKashitsyna29@users.noreply.github.com>
2023-05-15 01:58:34 +0300

    Update laba6.py

--- laba6.py
+++ laba6.py
@@ -24,3 +24,35 @@ def n2():
                 a+=k
     print('слово стоит', a)
 n2()
+
+def z3():
+    studies={
+        'A' : ['eng','rus','spa'],
+        'B': ['eng', 'rus', 'chi'],
+        'C': ['eng', 'rus'],
+        'D': ['rus', 'chi'],
+        'E': ['rus'],
+        'F': ['eng', 'spa'],
+        'G': ['kz', 'rus'],
+        'H': ['eng', 'it'],
+        'I': ['eng', 'fr'],
+    }
+    d = []
+    c = 0
+    r=[]
+    for i in studies:
+        for i in studies[i]:
+            if i not in d:
+                d.append(i)
+                c+=1
+    for i in studies:
+        if 'chi' in studies[i]:
+            r.append(i)
+        else:
+            continue
+    print('n3:')
+    print(*d)
+    print('всего языков:',c)
+    print(*(sorted(d)))
+    print('знают китайский', *r)
+z3()
